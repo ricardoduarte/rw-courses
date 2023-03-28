261e384b3d8abd4956cf471e8e6a28b1669c735e
Ricardo Duarte <rickduarte@gmail.com>
2023-03-28 11:28:06 -0300

    make a network call

--- rwcourses/lib/model/course.dart
+++ rwcourses/lib/model/course.dart
@@ -2,6 +2,23 @@ class Course {
   final String courseId;
   final String name;
   final String description;
+  final String difficulty;
+  final String artworkUrl;
+  final String contributors;
 
-  Course(this.courseId, this.name, this.description);
+  Course(this.courseId, this.name, this.description, this.difficulty,
+      this.artworkUrl, this.contributors);
+
+  Course.fromJson(Map<String, dynamic> json)
+      : courseId = json['id'] as String,
+        name = json['attributes']['name'] as String,
+        description = json['attributes']['description_plain_text'] as String,
+        difficulty = json['attributes']['difficulty'] as String,
+        artworkUrl = json['attributes']['card_artwork_url'] as String,
+        contributors = json['attributes']['contributor_string'] as String;
+
+  @override
+  String toString() {
+    return '$name: $difficulty';
+  }
 }
--- rwcourses/lib/repository/course_repository.dart
+++ rwcourses/lib/repository/course_repository.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:rwcourses/constants.dart';
 import 'package:rwcourses/model/course.dart';
 
@@ -5,7 +8,7 @@ import 'repository.dart';
 
 class CourseRepository implements Repository {
   String dataURL =
-      'https://api.raywanderlich.com/api/contents?filter[content_types][]=collection';
+      'https://api.kodeco.com/api/contents?filter[content_types][]=collection';
 
   @override
   Future<List<Course>> getCourses(int domainFilter) async {
@@ -14,6 +17,14 @@ class CourseRepository implements Repository {
 
     if (domainFilter != Constants.allFilter) {
       url += ';filter[domain_ids][]=$domainFilter';
+    }
+
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final apiResponse = json.decode(response.body) as Map<String, dynamic>;
+    final data = apiResponse['data'] as List;
+    for (final json in data) {
+      courses.add(Course.fromJson(json as Map<String, dynamic>));
     }
 
     return courses;
